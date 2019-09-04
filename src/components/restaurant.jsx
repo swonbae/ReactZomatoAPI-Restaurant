a27a66fa093ca8c33a9cacacfb9c376832683c91
swonb <swonbinca@gmail.com>
2019-09-04 01:05:28 -0400

    Initial Commit with dummy api (test)

---
+++ src/components/restaurant.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class Restaurant extends Component {}
+
+export default Restaurant;
---
+++ src/components/restaurantList.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import Restaurant from "./restaurant";
+
+class RestaurantList extends Component {}
+
+export default RestaurantList;
